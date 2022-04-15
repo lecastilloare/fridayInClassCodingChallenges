bb070fdf85b34250d67fd1166d9e280d6addefcc
Lee Castillo Arenas <leecastilloare@MacBook-Pro.local>
2022-04-15 13:43:09 -0400

    I am just pushing my code to my repo since I just created the file

---
+++ april_15_2022.js
@@ -0,0 +1,26 @@
+
+
+
+// Below is the js code for Question 1: 
+
+const balance = 100;
+const amountToBeMoved;
+
+const optionSelected = prompt("Please type one of the following: Deposit Money OR Withdraw Money");
+
+if (optionSelected == "Deposit Money" || "deposit money") {
+    amountToBeMoved = prompt("How much would you like to deposit?");
+    alert(`You're new balance is: ${}`)
+
+} else if (optionSelected == "Withdraw Money" || "withdraw money") {
+    prompt("How much would you like to withdraw")
+} else {
+    alert("Please select a valid option or kindly check your spelling (:")
+}
+
+
+// Below is the js code for Question 2:
+
+
+
+// Below is the js code for Question 3:
