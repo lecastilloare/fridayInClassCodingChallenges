8dc99c0f4f41c743863b1e21d71f0e75caf6be78
Lee Castillo Arenas <leecastilloare@MacBook-Pro.local>
2022-04-15 13:57:20 -0400

    Fixing code on the main branch because i'm pressed for time

--- april_15_2022.js
+++ april_15_2022.js
@@ -1,26 +1,22 @@
 
 
 
-// Below is the js code for Question 1: 
+// Below is the js code for Question 1,2,3: 
 
-const balance = 100;
-const amountToBeMoved;
-
-const optionSelected = prompt("Please type one of the following: Deposit Money OR Withdraw Money");
+const balance = 50;
+let amountToBeMoved;
+let total;
+let optionSelected = prompt("Please type one of the following: Deposit Money OR Withdraw Money");
 
 if (optionSelected == "Deposit Money" || "deposit money") {
-    amountToBeMoved = prompt("How much would you like to deposit?");
-    alert(`You're new balance is: ${}`)
+    amountToBeMoved = Number(prompt("How much would you like to deposit?"));
+    total = balance + amountToBeMoved;
+    alert(`Your new balance is: ${total}`);
 
 } else if (optionSelected == "Withdraw Money" || "withdraw money") {
-    prompt("How much would you like to withdraw")
+    amountToBeMoved = Number(prompt("How much would you like to withdraw?"));
+    total = balance - amountToBeMoved;
+    alert(`Your new balance is: ${total}`);
 } else {
     alert("Please select a valid option or kindly check your spelling (:")
 }
-
-
-// Below is the js code for Question 2:
-
-
-
-// Below is the js code for Question 3:
