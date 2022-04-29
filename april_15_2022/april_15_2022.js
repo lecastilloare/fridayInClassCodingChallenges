7bcd65dd50bfe8b05611c0ce64db3fd9d55dbf25
Lee Castillo Arenas <leecastilloare@MacBook-Pro.local>
2022-04-29 14:21:43 -0400

    updating old challenges to proper folders

---
+++ april_15_2022/april_15_2022.js
@@ -0,0 +1,47 @@
+
+
+
+// // Below is the js code for Question 1, 2, 3:
+
+// const balance = 100;
+// let total;
+// let entryAmount;
+// let optionSelected = prompt("Please type one of the following: Deposit Money OR 2 Withdraw Money");
+
+// if (optionSelected == ("Deposit Money" || "deposit money")) {
+//     entryAmount = Number(prompt("How much would you like to deposit?"));
+//     total = balance + entryAmount;
+//     alert(`Your new balance is: ${balance + entryAmount}`);
+
+// } else if (optionSelected == ("Withdraw Money" || "withdraw money")) {
+//     entryAmount = Number(prompt("How much would you like to withdraw?"));
+//     total = balance + entryAmount;
+//     alert(`Your new balance is: ${balance - entryAmount}`);
+
+// } else {
+//     alert("Please select a valid option or kindly check your spelling (:");
+// }
+
+
+// if (total > 0) {
+//     alert("You got monnnneeeeyyy!! Apparently it's payday for you.")
+// } else if (total < 0) {
+//     alert("You have low funds my man");
+// } else {
+//     alert("Your balance is 0");
+// }
+
+// const x = 40;
+
+// if (x % 10 == 0) {
+//     console.log("Divisible by 10”) ; else {
+//     console.log("Not divisible by 10”);
+// }
+
+
+const x = 40;
+if (x % 10 == 0) {
+    console.log("Divisible by 10")
+} else {
+    console.log("NOT DIV")
+}
